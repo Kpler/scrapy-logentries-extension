db7795c345c1039c3940192a3339dc7f53f6e928
Jean Maynier <jmaynier@kpler.com>
2015-07-20 20:27:38 +0200

    fix(context): set loagerAdapter when spider open

--- scrapylogentries/extension.py
+++ scrapylogentries/extension.py
@@ -6,22 +6,13 @@ logger = logging.getLogger(__name__)
 
 class LogentriesExtension(object):
 
-    def __init__(self, token, spider):
+    def __init__(self, token):
         self.token = token
         from logentries import LogentriesHandler
         import logging
         logging.info('Logentries activated with toker {}'.format(token))
         logging.root.addHandler(LogentriesHandler(token))
 
-        # Replace spider logger by LoggerAdapter giving more context if scrapy-job-parameter-extension is activated on Scrapinghub
-        if spider.spider_id is not None :
-            spider.logger = LogentriesAdapter(
-                logger=spider.logger,
-                extra={'spider_name': spider.name,
-                        'spider_id': spider.spider_id,
-                        'job_id': spider.job_id,
-                }
-            )
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -33,7 +24,21 @@ class LogentriesExtension(object):
 
 
         # instantiate the extension object
-        ext = cls(token, crawler.spider)
+        ext = cls(token)
+
+        crawler.signals.connect(ext.spider_opened, signal=signals.spider_opened)
+
 
         # return the extension object
         return ext
+
+    def spider_opened(self, spider):
+        # Replace spider logger by LoggerAdapter giving more context if scrapy-job-parameter-extension is activated on Scrapinghub
+        if spider.spider_id is not None:
+            spider.logger = LogentriesAdapter(
+                logger=spider.logger,
+                extra={'spider_name': spider.name,
+                       'spider_id': spider.spider_id,
+                       'job_id': spider.job_id,
+                       }
+            )
